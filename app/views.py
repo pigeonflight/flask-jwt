d27c686c5a19c8de8a43778bd7e9e8d2bc18b90c
David Bain <pigeonflight@gmail.com>
2016-04-05 14:09:13 +0000

    initial commit

---
+++ app/views.py
@@ -0,0 +1,125 @@
+"""
+Flask Documentation:     http://flask.pocoo.org/docs/
+Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
+Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
+
+This file creates your application.
+"""
+
+from app import app
+from flask import render_template, request, redirect, url_for
+import jwt
+import base64
+import os
+
+from functools import wraps
+from flask import Flask, request, jsonify, _request_ctx_stack
+from werkzeug.local import LocalProxy
+from flask.ext.cors import cross_origin
+
+# Authentication annotation
+current_user = LocalProxy(lambda: _request_ctx_stack.top.current_user)
+
+# Authentication attribute/annotation
+def authenticate(error):
+  resp = jsonify(error)
+
+  resp.status_code = 401
+
+  return resp
+
+def requires_auth(f):
+  @wraps(f)
+  def decorated(*args, **kwargs):
+    auth = request.headers.get('Authorization', None)
+    if not auth:
+      return authenticate({'code': 'authorization_header_missing', 'description': 'Authorization header is expected'})
+
+    parts = auth.split()
+
+    if parts[0].lower() != 'bearer':
+      return {'code': 'invalid_header', 'description': 'Authorization header must start with Bearer'}
+    elif len(parts) == 1:
+      return {'code': 'invalid_header', 'description': 'Token not found'}
+    elif len(parts) > 2:
+      return {'code': 'invalid_header', 'description': 'Authorization header must be Bearer + \s + token'}
+
+    token = parts[1]
+    try:
+        payload = jwt.decode(
+            token,
+            base64.b64decode('YOUR_CLIENT_SECRET'.replace("_","/").replace("-","+")),
+            audience='YOUR_CLIENT_ID'
+        )
+    except jwt.ExpiredSignature:
+        return authenticate({'code': 'token_expired', 'description': 'token is expired'})
+    except jwt.InvalidAudienceError:
+        return authenticate({'code': 'invalid_audience', 'description': 'incorrect audience, expected: YOUR_CLIENT_ID'})
+    except jwt.DecodeError:
+        return authenticate({'code': 'token_invalid_signature', 'description': 'token signature is invalid'})
+
+    _request_ctx_stack.top.current_user = user = payload
+    return f(*args, **kwargs)
+
+  return decorated
+
+###
+# Routing for your application.
+###
+
+@app.route('/')
+def home():
+    """Render website's home page."""
+    return render_template('home.html')
+
+
+@app.route('/about/')
+def about():
+    """Render the website's about page."""
+    return render_template('about.html')
+
+# Controllers API
+
+# This doesn't need authentication
+@app.route("/ping")
+@cross_origin(headers=['Content-Type', 'Authorization'])
+def ping():
+    return "All good. You don't need to be authenticated to call this"
+
+# This does need authentication
+@app.route("/secured/ping")
+@cross_origin(headers=['Content-Type', 'Authorization'])
+@requires_auth
+def securedPing():
+    return "All good. You only get this message if you're authenticated"
+
+###
+# The functions below should be applicable to all Flask apps.
+###
+
+@app.route('/<file_name>.txt')
+def send_text_file(file_name):
+    """Send your static text file."""
+    file_dot_text = file_name + '.txt'
+    return app.send_static_file(file_dot_text)
+
+
+@app.after_request
+def add_header(response):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
+    response.headers['Cache-Control'] = 'public, max-age=600'
+    return response
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True,host="0.0.0.0",port="8888")
